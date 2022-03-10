33542199b1807cbc96c814e85a94f272e0b0dd98
zyyKing <745771857@qq.com>
2022-03-10 10:35:57 +0800

    部分样式更正

--- src/components/Fuzzy/index.vue
+++ src/components/Fuzzy/index.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="w-full zinc rounded-sm h-full">
-    <Bar />
-    <Query />
-    <FTable />
-    <Pagination />
+    <Bar ref="bar"/>
+    <el-scrollbar
+        ref="scrollBar"
+        class="table-h-scrollbar">
+      <Query />
+      <FTable />
+      <Pagination />
+    </el-scrollbar>
   </div>
 </template>
 
@@ -12,7 +16,11 @@ import type { TemplateConfiguration } from './types'
 import { UpdateModelProvide } from './types'
 import { Bar, FTable, Pagination, Query } from './components'
 import { initialzeFuzzy } from './utils'
-
+const bar = ref()
+const scrollBar = ref()
+onMounted(() => {
+  scrollBar.value.$el.style.height = `calc(100% - ${bar.value.$el.clientHeight + 'px'})`
+})
 const props = defineProps({
   config: Object,
 })
